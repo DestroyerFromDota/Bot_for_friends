2aaf742ccf0b17bcbe5bdefdb932fe410d94b2a2
DestroyerFromDota <131293287+DestroyerFromDota@users.noreply.github.com>
2024-11-04 16:58:48 +0300

    Add files via upload

--- baseFileTelegrammBot.py
+++ baseFileTelegrammBot.py
@@ -2,8 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from bot_token import token as bot_token
-from connect_mySQL import *
+
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
@@ -25,18 +24,6 @@ async def process_start_command(message: Message):
 async def process_help_command(message: Message):
     await message.answer('эта функция пока не готова')
 
-
-# Этот хэндлер будет срабатывать на любые ваши сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
-    try:
-        await message.reply(text=f'{select_conn_answerDB(message.text.lower())}')
-        print(message)
-        print(message.text)
-    except TypeError:
-        await message.reply(text='Данный тип апдейтов не поддерживается '
-                                 'методом send_copy')
 
 
 if __name__ == '__main__':
